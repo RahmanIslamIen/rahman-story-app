b734d0b65028a09993d48a10a362491d89c455f0
rahmanislamien <rahmanislamien@gmail.com>
2024-01-25 22:40:11 +0700

    perbaikan

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
   },
-  mode: 'development',
-  entry: './index.js',
+  mode: "development",
+  entry: "./index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
@@ -20,18 +20,44 @@ module.exports = {
           loader: "babel-loader",
           options: {
             plugins: [
-              ["@babel/plugin-proposal-decorators", { decoratorsBeforeExport: true }],
-              ["@babel/plugin-proposal-class-properties", { "loose": true }]
-            ]
-          }
-        }
-      }
-    ]
+              [
+                "@babel/plugin-proposal-decorators",
+                { decoratorsBeforeExport: true },
+              ],
+              ["@babel/plugin-proposal-class-properties", { loose: true }],
+            ],
+          },
+        },
+      },
+      // Aturan untuk mengelola file CSS Bootstrap
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      // Aturan untuk mengelola file JavaScript Bootstrap
+      {
+        test: /bootstrap\.js$/,
+        use: {
+          loader: "imports-loader",
+          options: {
+            jQuery: "jquery",
+          },
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      chunksSortMode: 'none',
-      template: 'index.html'
+      chunksSortMode: "none",
+      template: "index.html",
     }),
   ],
 };
