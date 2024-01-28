baf3798861b54e9c23e86ed135d00b8bdc7b0e41
rahmanislamien <rahmanislamien@gmail.com>
2024-01-28 13:39:16 +0700

    perbaikan js boostrap ke 1

--- views/main-view.js
+++ views/main-view.js
@@ -8,6 +8,7 @@ class bagianNavbar extends LitElement {
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
       />
+
       <nav class="navbar navbar-expand-lg" style="background-color: #20c997;">
         <div class="container-fluid">
           <a class="navbar-brand" href="#"><b>📰 Rahman Story APP</b></a>
@@ -39,6 +40,8 @@ class bagianNavbar extends LitElement {
           </div>
         </div>
       </nav>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     `;
   }
 }
